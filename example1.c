fe6456f0720ea9f428b7bf0cad6e55f3a1d8aa7d
sasikumarnsk <61141651+sasikumarnsk@users.noreply.github.com>
2020-02-28 15:45:44 +0530

    Add files via upload

---
+++ example1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>    //Preprocessor Section
+#include<conio.h>
+int main()          //main function
+{
+  int n=5;           //Declaration section
+  for(i=0;i<n;i++)
+  {
+    printf("HEllo World\n");      //Executable part
+  }
+  return 0;
+}
